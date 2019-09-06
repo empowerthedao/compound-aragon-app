225d389d00983d1c3a82f8e4d9c1079f545f9f1a
William Griffiths <willjgriff@gmail.com>
2019-09-06 18:08:15 +0100

    Added TokenSelector for deposit tab in Transfer side panel.

--- compound-aragon-app/app/src/components/balances/side-panel-input/Deposit.js
+++ compound-aragon-app/app/src/components/balances/side-panel-input/Deposit.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Info, DropDown, Field, TextInput, Button} from '@aragon/ui'
 import styled from "styled-components";
+import TokenSelector from "./token-selector/TokenSelector";
 
 const CUSTOM_TOKEN_DECIMALS = -1
 
@@ -11,11 +12,6 @@ const Deposit = ({tokens, handleDeposit}) => {
     const [otherToken, setOtherToken] = useState("")
     const [amount, setAmount] = useState(0)
 
-    const tokensAvailable = tokens.map(token => token.name)
-    tokensAvailable.push("Other...")
-
-    const showOtherTokenInput = tokenSelected === tokensAvailable.length - 1
-
     const getSelectedTokenAddress = () => showOtherTokenInput ? otherToken : tokens[tokenSelected].address
     const getSelectedTokenDecimals = () => showOtherTokenInput ? CUSTOM_TOKEN_DECIMALS : tokens[tokenSelected].decimals
 
@@ -24,26 +20,17 @@ const Deposit = ({tokens, handleDeposit}) => {
         handleDeposit(getSelectedTokenAddress(), amount, getSelectedTokenDecimals())
     }
 
+    const showOtherTokenInput = tokenSelected === tokens.length
+
     return (
         <form onSubmit={handleSubmit}>
             <DepositContainer>
 
-                <FieldStyled label="Token">
-                    <DropDown items={tokensAvailable}
-                              required
-                              onChange={selectedTokenIndex => setTokenSelected(selectedTokenIndex)}
-                              selected={tokenSelected}
-                              wide/>
-                </FieldStyled>
-
-                {showOtherTokenInput && (
-                    <FieldStyled label="Token Address">
-                        <TextInput type="text"
-                                   wide
-                                   required={showOtherTokenInput}
-                                   onChange={event => setOtherToken(event.target.value)}/>
-                    </FieldStyled>
-                )}
+                <TokenSelector onSelectToken={setTokenSelected}
+                               onOtherInput={setOtherToken}
+                               tokens={tokens}
+                               selectedIndex={tokenSelected}
+                               wide/>
 
                 <FieldStyled label="Amount">
                     <TextInput type="number"
